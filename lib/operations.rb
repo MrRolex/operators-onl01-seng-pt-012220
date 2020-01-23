e13bd6672312bca118b9eee2ec75f687a2085069
Edward Williams <eddie_w38@yahoo.com>
2020-01-23 11:14:18 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,4 +1,16 @@
 def unsafe?(speed)
+  if speed < 40
+    puts "You are speeding"
+    
+  elsif speed => 30
+  puts "Your driving safe"
+  
+else
+  puts "Not valid speed"
+  
+end
+  
+    
 
 end
 
